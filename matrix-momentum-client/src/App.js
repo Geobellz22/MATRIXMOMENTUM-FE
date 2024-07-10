6c6e484fd1350602b8bbea1269ec70ad3dcdae46
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2024-07-10 12:24:08 +0100

    Update App.js branch

--- matrix-momentum-client/src/App.js
+++ matrix-momentum-client/src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Profile from './components/Profile';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import OrderHistory from './components/OrderHistory';
+import OrderTracking from './components/OrderTracking';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <nav>
-            <Link to="/">Home</Link> | 
-            <Link to="/login">Login</Link> | 
-            <Link to="/register">Register</Link> | 
-            <Link to="/profile">Profile</Link>
-          </nav>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+        <header>
+          {/* Removed navigation links */}
         </header>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order-history" element={<OrderHistory />} />
+          <Route path="/order-tracking/:orderId" element={<OrderTracking />} />
+        </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
